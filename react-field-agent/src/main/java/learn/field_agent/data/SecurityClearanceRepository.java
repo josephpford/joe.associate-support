0c07dba71a4563a3358880fe62890a2b3fec0152
Joe Ford <josephpford@gmail.com>
2022-06-28 14:15:16 -0500

    Removing field agent

--- react-field-agent/src/main/java/learn/field_agent/data/SecurityClearanceRepository.java
+++
@@ -1,7 +0,0 @@
-package learn.field_agent.data;
-
-import learn.field_agent.models.SecurityClearance;
-
-public interface SecurityClearanceRepository {
-    SecurityClearance findById(int securityClearanceId);
-}

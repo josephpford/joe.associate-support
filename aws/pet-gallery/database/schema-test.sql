445f0550d3d81cb5f6bb0b40168c6798e81228f8
oneowhat <brendan.kendrick@protonmail.com>
2022-12-14 10:15:21 -0600

    updated schema

--- aws/pet-gallery/database/schema-test.sql
+++ aws/pet-gallery/database/schema-test.sql
@@ -5,5 +5,5 @@ use pet_gallery_test;
 create table pet (
 	pet_id int primary key auto_increment,
     `name` varchar(100) not null,
-    image_url varchar(250) not null
+    image_url varchar(255) not null
 );
